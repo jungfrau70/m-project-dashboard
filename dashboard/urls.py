2f4f40e689f52f9e36010b56cc0de4b170449711
InHwan Jung <inhwan.jung@gmail.com>
2022-06-23 03:15:37 -0700

    2022_06_23_03_15_AM

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='dashboard'),
+    path('', views.main, name='main'),
+    path('dashboard/', views.IndexView.as_view(), name='dashboard'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,26 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
+from vega_datasets import data
+import altair as alt
 
 # Create your views here.
 
 def main(request): #메인 화면
     return render(
     request, 
-    'index.html', 
+    'home.html', 
     {})
+
+class IndexView(TemplateView):
+    template_name = 'dashboard.html'
+
+    def get(self, request, *args, **kwargs):
+        context = locals()
+        source = data.cars()
+
+        context['chart'] = alt.Chart(source).mark_circle().encode(
+            x='Horsepower',
+            y='Miles_per_Gallon',
+            color='Origin'
+        ).interactive()
+        return render(request, self.template_name, context)    
